74727211070c4a08f5f89e767db301a6542befeb
ChoBaeKR <tmsprqo@gmail.com>
2022-04-04 21:23:03 +0900

    22.04.04 백준 이분탐색 풀이

--- backjoon/silver/1654.py
+++ backjoon/silver/1654.py
@@ -1,20 +1,23 @@
 # 랜선 자르기 이분탐색 공부하자
-k, n = map(int, input().split())    # k(현재 랜선의 개수), n(필요한 랜선의 개수)
+from sys import stdin
+k, n = map(int,stdin.readline().split())    # k(현재 랜선의 개수), n(필요한 랜선의 개수)
 answer = 0
 lanLi = [int(input()) for _ in range(k)]
 lanLi.sort()
-start, end = 0, 0
+start, end = 1, sum(lanLi)//n
 
-while start != 0: 
-    if answer == n:
-        break
+while end-start>= 0:
+    mid = (end - start) // 2 
+    tmp = 0
     
-    elif n >= lanLi[start]:
-        n -= lanLi[start]
-        start += 1
-        end += 1
+    for lan in lanLi:
+        tmp += lan // mid
+    
+   
+    if tmp >= n:
+        start = mid + 1
+        answer = mid
     else:
-        start += 1
-        end += 1
-        n -= lanLi[start]   
-print(tmp)
+        end = mid - 1
+
+print(answer)
--- backjoon/silver/1920.py
+++ backjoon/silver/1920.py
@@ -1,27 +1,30 @@
-# 수찾기 실버4
-
-answers = []
+# 수찾기 실버4 이분탐색
+# 입력
 n = int(input())
 nLi = list(map(int, input().split()))
 m = int(input())
 mLi = list(map(int, input().split()))
+# 이분탐색을 위한 정렬
 nLi.sort()
-mLi.sort()
-
+#  이분 탐색 과정
 def binary(num):
+    # 시작, 끝값 설정
     start= 0; 
     end= len(nLi)-1; 
+    # 탐색까지 반복
     while end-start>= 0:
-        
-        mid = (start+end)/2
-        
+        mid = (start+end)//2
         if nLi[mid] == num:
             return True
-        if nLi[mid] < num:
-            start = mid+1
         else:
-            end = mid-1
-
+            # 중간값보다 크면 시작값을 중간에 오른쪽 수(큰수)로 바꿈
+            if nLi[mid] < num:
+                start = mid+1
+            # 중간값보다 작으면 끝값을 중간값의 왼쪽 수(작은수)로 바꿈 
+            else:
+                end = mid-1
+    return False
+# 출력
 for numM in mLi:
     if binary(numM):
         print(1)
