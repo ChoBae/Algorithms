9deed444bda09761e8dac330f3c985bc8d8fcf7a
ChoBaeKR <tmsprqo@gmail.com>
2022-04-12 22:41:42 +0900

    22.04.12 백준

--- backjoon/silver/2609.py
+++ backjoon/silver/2609.py
@@ -1,9 +1,18 @@
 # 최대공약수와 최소공배수
+from math import gcd
+
+
 num1, num2 = map(int, input().split())
-24 -> [1,2,3,4,6,8,12,24]
-18 -> [1,2,3,6,9,18
-       
-       num1 /= 2
-       
-       if 
-    
+# 24 -> [1,2,3,4,6,8,12,24]
+# 18 -> [1,2,3,6,9,18]
+num1L = []
+num2L = []
+for i in range(1,num1+1):
+    if num1 % i == 0:
+       num1L.append(int(i))
+    if num2 % i == 0:
+       num2L.append(int(i))
+
+result = [i for i in num2L if i in num1L]
+print(max(result))
+print((num1*num2)// max(result))
