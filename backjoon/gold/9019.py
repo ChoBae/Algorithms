50253d214bf392667931dec2cc7965e559eaf1bc
ChoBaeKR <tmsprqo@gmail.com>
2022-04-22 21:50:24 +0900

    22.04.22 백준

---
+++ backjoon/gold/9019.py
@@ -0,0 +1,53 @@
+# DSLR
+from collections import deque
+import sys
+input = sys.stdin.readline
+# 입력
+t = int(input())    # 테스트 케이스 수
+cases = ['D','S','L','R']
+# DSLR 각각 연산 리턴
+def state(num, case):
+    if case == 'D':
+        return((int(num)*2) % 10000)
+    elif case == 'S':
+        return (int(num)-1) % 10000
+    elif case == 'L':
+        # 첫자리 뒤로보내기
+        tmp = num // 1000
+        return num % 1000 * 10 + tmp
+    elif case =='R':
+        # 뒷자리 앞으로 보내기
+        tmp = num % 10
+        return num // 10 + 1000 * tmp
+    
+# BFS 솔루션
+def bfs(num, target, visited):
+    # 시작수와 빈문자열로 시작
+    queue = deque([[num,""]])
+    # 방문처리
+    visited[num] = 1
+    # bfs 체크
+    while queue:
+        # 숫자와 case(연산작업)
+        number , case = queue.popleft()
+        print(number , case)
+        # 타겟을 찾았을때 -> 결과
+        if number == target:
+            print(case)
+            break
+        # 현재 숫자에서 DSLR 다적용.
+        for i in range(4):
+            caseNum = state(number,cases[i])
+            # 방문 처리
+            if visited[caseNum] == 0:
+                # 연산 더해주면서 넣어주기.
+                queue.append((caseNum, case + cases[i]))
+                visited[caseNum] = 1
+# 테스트 케이스 반복
+for _ in range(t):
+    # 방문 체크 리스트
+    visited= [0 for _ in range(10000)]
+    # 시작 수, 타겟 수
+    num, target = map(int,input().split())
+    # bfs 솔루션
+    bfs(num,target,visited)
--- backjoon/silver/2164.py
+++ backjoon/silver/2164.py
@@ -1,29 +1,29 @@
 # 카드2 실버4
 from collections import deque
-# n = int(input())
-# cardLi = [i+1 for i in range(n)]
-# cardLi = deque(cardLi)
-# while len(cardLi) != 1:
-#     # 카드를 땅에 버림->pop보다 더 시간복잡도가 좋네
-#     # 리스트에서 pop(0)으로 가장 앞단의 값을 꺼낼때 list크기만큼 시간이 걸린다(O(n))
-#     # 하지만 deque에서 popleft()의 경우 똑같은 pop(0)인데 O(1)의 시간복잡도.
-#     # -> index값을 알경우에는 결국 똑같긴하다.
-#     cardLi.popleft()
-#     # 카드를 꺼내서 맨밑으로 넣음
-#     setCard = cardLi.popleft()
-#     cardLi.append(setCard)
-# print(cardLi[0])
-
-# 인덱스로 뺄경우 ->시간초과
 n = int(input())
-cardStr = ''
-for i in range(n):
-    cardStr += str(i+1)
-while len(cardStr) != 1:
-    # 첫번째제거
-    cardStr = cardStr[1:]
-    # 그이후오는수 뒤로빼기
-    setCard = cardStr[0]
-    cardStr = cardStr[1:] + setCard
-print(cardStr)
+cardLi = [i+1 for i in range(n)]
+cardLi = deque(cardLi)
+while len(cardLi) != 1:
+    # 카드를 땅에 버림->pop보다 더 시간복잡도가 좋네
+    # 리스트에서 pop(0)으로 가장 앞단의 값을 꺼낼때 list크기만큼 시간이 걸린다(O(n))
+    # 하지만 deque에서 popleft()의 경우 똑같은 pop(0)인데 O(1)의 시간복잡도.
+    # -> index값을 알경우에는 결국 똑같긴하다.
+    cardLi.popleft()
+    # 카드를 꺼내서 맨밑으로 넣음
+    setCard = cardLi.popleft()
+    cardLi.append(setCard)
+print(cardLi[0])
+
+# # 인덱스로 뺄경우 ->시간초과
+# n = int(input())
+# cardStr = ''
+# for i in range(n):
+#     cardStr += str(i+1)
+# while len(cardStr) != 1:
+#     # 첫번째제거
+#     cardStr = cardStr[1:]
+#     # 그이후오는수 뒤로빼기
+#     setCard = cardStr[0]
+#     cardStr = cardStr[1:] + setCard
+# print(cardStr)
 
