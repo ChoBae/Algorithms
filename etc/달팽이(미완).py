e29ac820000ab98fe2805a70323c4f4662350dc4
ChoBaeKR <101175828+ChoBaeKR@users.noreply.github.com>
2022-05-04 19:00:08 +0900

    22.05.04 달팽이 나열 연습

---
+++ etc/달팽이(미완).py
@@ -0,0 +1,27 @@
+# 달팽이 알고리즘
+# 5x5의 배열이 주어졌다 치고
+x, y = 0, 0
+maps = [[0] * 5 for i in range(5)]
+stateNum = 1
+task = ['right','down', 'left', 'up']
+taskNum = 0
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+for i in range(1,25):
+
+        
+
+    maps[x][y] = stateNum
+    stateNum += 1
+    if i % 5 == 0:
+        taskNum+= 1
+    if taskNum == 4:
+        taskNum = 0
+        
+    x = x + dx[taskNum]
+
+    y = y + dy[taskNum]
+
+    
+    print(maps)
