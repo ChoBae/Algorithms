6a88838cb1ae335de9a4276ff6010e04c3dfc7d0
ChoBaeKR <tmsprqo@gmail.com>
2022-05-20 22:59:21 +0900

    22.05.20 SWEA 풀이

---
+++ SWEA/D2/1983.py
@@ -0,0 +1,4 @@
+# 조교의 성적매기기
+t = int(input())
+for tc in range(1,t+1):
+    
--- SWEA/D3/1215.py
+++ SWEA/D3/1215.py
@@ -1,16 +1,36 @@
 # 회문 1
-t = 1
+t = 10
 for tc in range(1, t+1):
-    targetLen = int(input())
+    targetLen = int(input())  # 회문 크기
+    # 이동 방향(오른쪽, 아래)
     dx = [0, targetLen]
     dy = [targetLen, 0]
-    maps = [list(input()) for _ in range(8)]
+    ans = 0  # 정답 수 초기화
+    maps = [list(input()) for _ in range(8)]    # 맵 데이터
+    # 맵 위치 완전탐색 좌표 (i,j)
     for i in range(8):
         for j in range(8):
-            # 현재 위치에서 두가지 방향
+            # 현재 위치에서 두가지 방향(오른쪽, 아래)
             for z in range(2):
+                tmp = ''    # 회문 데이터
+                # 회문 길이만큼의 좌표
                 nx = i + dx[z]
                 ny = j + dy[z]
-                if 0<= nx <=7 and 0 <= ny <=7:
-                    print(maps[i:i+nx][j:j+ny]) 
-            
+                # 이동가능한 좌표일때
+                if 0 <= nx <= 8 and 0 <= ny <= 8:
+                    # 오른쪽 방향
+                    if z == 0:
+                        for x in range(targetLen):
+                            tmp += maps[i][j+x]
+                        # 회문일때
+                        if tmp == tmp[::-1]:
+                            ans += 1
+                    # 아래 방향
+                    else:
+                        for x in range(targetLen):
+                            tmp += maps[i+x][j]
+                        # 회문일때
+                        if tmp == tmp[::-1]:
+                            ans += 1
+    # 출력
+    print(f"#{tc} {ans}")
