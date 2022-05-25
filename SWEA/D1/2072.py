c284fdc25b262c6d61d919586c4a706f2967b605
ChoBaeKR <101175828+ChoBaeKR@users.noreply.github.com>
2022-05-25 14:56:29 +0900

    22.05.25 pep8 작성

--- SWEA/D1/2072.py
+++ SWEA/D1/2072.py
@@ -1,12 +1,14 @@
 # 홀수만 더하기
 # 홀수 체크 함수
 def oddNum(num):
-    if num % 2 ==0:
+    if num % 2 == 0:
         return False
     return True
+
+
 # 입력
 T = int(input())
-for test_case in range(1, T + 1):
+for test_case in range(1, T+1):
     numLi = list(map(int, input().split()))
     result = 0
     # numLi 체크
