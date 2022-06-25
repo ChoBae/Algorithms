4a1134751f2d0c5ef5640fd8423667ecabceafa3
chobae <tmsprqo@gmail.com>
2022-06-25 17:33:33 +0900

    22.06.25 백준 풀이

--- backjoon/gold/9019.py
+++ backjoon/gold/9019.py
@@ -1,4 +1,4 @@
-# DSLR
+# DSLR -> pypy3 정답처리.. 보통 python
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -14,23 +14,22 @@ def state(num, case):
     elif case == 'L':
         # 첫자리 뒤로보내기
         tmp = num // 1000
-        return num % 1000 * 10 + tmp
-    elif case =='R':
+        return (num % 1000 * 10 + tmp) % 10000
+    else:
         # 뒷자리 앞으로 보내기
         tmp = num % 10
-        return num // 10 + 1000 * tmp
+        return (num // 10 + 1000 * tmp) % 10000
     
 # BFS 솔루션
 def bfs(num, target, visited):
     # 시작수와 빈문자열로 시작
     queue = deque([[num,""]])
     # 방문처리
-    visited[num] = 1
+    visited[num] = True
     # bfs 체크
     while queue:
         # 숫자와 case(연산작업)
         number , case = queue.popleft()
-        print(number , case)
         # 타겟을 찾았을때 -> 결과
         if number == target:
             print(case)
@@ -39,14 +38,14 @@ def bfs(num, target, visited):
         for i in range(4):
             caseNum = state(number,cases[i])
             # 방문 처리
-            if visited[caseNum] == 0:
+            if not visited[caseNum]:
                 # 연산 더해주면서 넣어주기.
                 queue.append((caseNum, case + cases[i]))
-                visited[caseNum] = 1
+                visited[caseNum] = True
 # 테스트 케이스 반복
 for _ in range(t):
     # 방문 체크 리스트
-    visited= [0 for _ in range(10000)]
+    visited= [False] * 10000
     # 시작 수, 타겟 수
     num, target = map(int,input().split())
     # bfs 솔루션
--- backjoon/gold/9019_1.py
+++ backjoon/gold/9019_1.py
@@ -1,5 +1,8 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 dslr = ["D", 'S', 'L', 'R']
+
 def state(num, state):
     if state == 'D':
         tmp = num * 2
@@ -21,7 +24,7 @@ def bfs(start, target, visited):
     while q:
         nowNum, nowState = q.popleft()
         if int(nowNum) == int(target):
-            print(nowState)
+            answer.append(nowState)
             break
         for i in range(len(dslr)):
             newState = nowState + dslr[i]
@@ -32,7 +35,11 @@ def bfs(start, target, visited):
             
         
 t = int(input())
+answer = []
 for _ in range(t):
     visited = [0 for _ in range(10000)]
     a, b = input().split()
     bfs(a, b, visited)
+
+for i in range(t):
+    print(answer[i])
