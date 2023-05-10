3afc49fb3029e5f2d1aa6449176b83742b7f1787
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-05-10 17:18:28 +0900

    sol : stack nodeVer

---
+++ LiveCoding/DataStructures/stack_node_ver.js
@@ -0,0 +1,36 @@
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(value) {
+    const curNode = new Node(value);
+    if (this.top) curNode.next = this.top;
+    this.top = curNode;
+  }
+  pop() {
+    if (!this.top) return console.log("스택이 비었어요.");
+    const value = this.top.value;
+    this.top = this.top.next;
+    return value;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack);
